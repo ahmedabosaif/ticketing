dffb3efbb5a5ce387ee46e8da800cc8f508e2c8e
ahmedabosaif <ahmed.t.abosaif@gmail.com>
2022-03-02 20:00:45 +0200

    Updates

---
+++ precache-manifest.7706e3a1ab46f7880597caf3891125c1.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7c57c114727e546328ca20e688551e",
+    "url": "/ticketing/index.html"
+  },
+  {
+    "revision": "6cf96bbf538e85a6ed54",
+    "url": "/ticketing/static/css/2.8edfb10f.chunk.css"
+  },
+  {
+    "revision": "83089a71b7ce394c59c8",
+    "url": "/ticketing/static/css/main.304196fe.chunk.css"
+  },
+  {
+    "revision": "6cf96bbf538e85a6ed54",
+    "url": "/ticketing/static/js/2.37366d5c.chunk.js"
+  },
+  {
+    "revision": "5743e25cfb6af70046528eaa9b35c2c5",
+    "url": "/ticketing/static/js/2.37366d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83089a71b7ce394c59c8",
+    "url": "/ticketing/static/js/main.07e09e41.chunk.js"
+  },
+  {
+    "revision": "6e0680e49eaca0aff96187570bc4ffc9",
+    "url": "/ticketing/static/js/main.07e09e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38de5fc353193bdb706d",
+    "url": "/ticketing/static/js/runtime-main.524209cc.js"
+  },
+  {
+    "revision": "20b238d454fe547904a42e1044cff1b0",
+    "url": "/ticketing/static/media/App.20b238d4.png"
+  },
+  {
+    "revision": "169cde85802ea430ae2a4a8eb363a0c7",
+    "url": "/ticketing/static/media/Computer.169cde85.png"
+  },
+  {
+    "revision": "6a037b687197db0740515dbc4989bb35",
+    "url": "/ticketing/static/media/File.6a037b68.png"
+  },
+  {
+    "revision": "5aceaa069a2fb0ba365501f3e51a269a",
+    "url": "/ticketing/static/media/Mail.5aceaa06.png"
+  },
+  {
+    "revision": "bce60030d4b369949971f883a75095c1",
+    "url": "/ticketing/static/media/Network.bce60030.png"
+  },
+  {
+    "revision": "02d82fb4f94018c5202ad77c54a812d2",
+    "url": "/ticketing/static/media/Printer.02d82fb4.png"
+  },
+  {
+    "revision": "db87164209f5d92030237757ed3f1650",
+    "url": "/ticketing/static/media/Sap.db871642.png"
+  },
+  {
+    "revision": "7d6ad242c2c21041a920f289cc74d2ac",
+    "url": "/ticketing/static/media/SelfServicePortalBanne1r.7d6ad242.jpg"
+  },
+  {
+    "revision": "1ad2c1e60ae70cebb3607a930df5cfe1",
+    "url": "/ticketing/static/media/V3.1ad2c1e6.pdf"
+  },
+  {
+    "revision": "8be58096fb484ba8da4c6b1b138c255f",
+    "url": "/ticketing/static/media/bg.8be58096.jpg"
+  },
+  {
+    "revision": "2cfc05f64d299579ea80e7057a512f0d",
+    "url": "/ticketing/static/media/bkg-gry.2cfc05f6.jpg"
+  },
+  {
+    "revision": "53a658e5769b54fa9cd19bd2d16e1198",
+    "url": "/ticketing/static/media/clipart-camera-security-camera-7-transparent.53a658e5.png"
+  },
+  {
+    "revision": "8e74c56aaf681406d9121b67676b44d8",
+    "url": "/ticketing/static/media/globe.8e74c56a.jpg"
+  },
+  {
+    "revision": "9ad52af83bc15294d3d4e49880a63c4e",
+    "url": "/ticketing/static/media/iP-Cam.9ad52af8.png"
+  },
+  {
+    "revision": "a3591d531ad4ae8591b2bfce15d90fe3",
+    "url": "/ticketing/static/media/iP-Phone.a3591d53.png"
+  },
+  {
+    "revision": "0a842203182a22896da13096982b6e28",
+    "url": "/ticketing/static/media/it-ticketing-system.0a842203.png"
+  },
+  {
+    "revision": "f78c172683426057db5a93cdd9363ba3",
+    "url": "/ticketing/static/media/itsm-software.f78c1726.png"
+  },
+  {
+    "revision": "5dd4f6f6cd967a8b540167cf11262ece",
+    "url": "/ticketing/static/media/logo_egyption_steel.5dd4f6f6.png"
+  },
+  {
+    "revision": "b6a8fad62a16761d73f442c4c3da0504",
+    "url": "/ticketing/static/media/map2.b6a8fad6.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/ticketing/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/ticketing/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/ticketing/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/ticketing/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "0a3ba1143b67851e4920023ec16ebfdf",
+    "url": "/ticketing/static/media/s1.0a3ba114.jpg"
+  }
+]);
